902ddc9ce90a4d83c0b77844dd5ae46f3f7a6c90
John Chan <john7chan@gmail.com>
2024-10-25 12:00:26 -0400

    stop timer and add confetti on win

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Crossword from '@jaredreisinger/react-crossword';
-import Stopwatch from './Stopwatch.tsx'
+import { useState, useEffect } from "react";
+import JSConfetti from 'js-confetti'
 
 const data = {
   across: {
@@ -57,10 +58,48 @@ const style = {
 }
 
 export default function App() {
-    return ( 
-      <div>
-        <Stopwatch/>
-        <Crossword data={data} theme={style}/>
+  let jsConfetti: JSConfetti;
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId = 0;
+    if (isRunning) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+  useEffect(() => {
+    startTimer();
+  }, []);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    jsConfetti = new JSConfetti();
+    setIsRunning(false);
+    jsConfetti.addConfetti();
+  };
+  
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  return ( 
+    <div>
+      <div className="stopwatch-container">
+        <p className="stopwatch-time">
+          {minutes.toString().padStart(2, "0")}:
+          {seconds.toString().padStart(2, "0")}
+        </p>
       </div>
-    )
+      <Crossword data={data} theme={style} onCrosswordCorrect={() => stopTimer()}/>
+    </div>
+  )
 }
